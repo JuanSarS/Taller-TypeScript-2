2dcbc42eb7e99043e8d22ae469c3e8e42c0a4b95
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2023-04-16 19:44:41 -0500

    Taller TypeScript 2

---
+++ Taller 2/Serie.ts
@@ -0,0 +1,12 @@
+export enum ej{
+    bach = "bach",
+    uni = "uni",
+    asd =" asd"
+}
+
+export class Serie {
+    constructor(public id:number ,public nombre:string,public productora:string, public temporadas:number , 
+        public reseña: string, public url:string, public imagen:string,public ej:ej ){
+
+    }
+}
